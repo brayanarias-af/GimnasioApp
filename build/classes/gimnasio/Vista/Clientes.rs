ffe8c0cdd675f68b8a89cc9bb0e6771ba3fbcddc
brayanarias-af <brayansneari09@gmail.com>
2026-05-15 06:30:35 -0500

    Delete build/classes/gimnasio directory

--- build/classes/gimnasio/Vista/Clientes.rs
+++
@@ -1,6 +0,0 @@
-Gimnasio.Vista.ClientesVista$1
-Gimnasio.Vista.ClientesVista$2
-Gimnasio.Vista.ClientesVista$AccionesCellRenderer
-Gimnasio.Vista.ClientesVista$3
-Gimnasio.Vista.ClientesVista
-Gimnasio.Vista.ClientesVista$AccionesCellEditor
